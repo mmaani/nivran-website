669638223830d6dd54cccbca250260dd9e0923ed
Mohammad Maani <mohammad.maani@hotmail.com>
2026-03-03 03:26:23 +0000

    Add audit outputs

---
+++ audit_outputs/preview_archive_rename.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+ALTER TABLE public.batches RENAME TO batches_archive_20260303;
+ALTER TABLE public.coupons RENAME TO coupons_archive_20260303;
+ALTER TABLE public.customer_cart_items RENAME TO customer_cart_items_archive_20260303;
+ALTER TABLE public.customer_carts RENAME TO customer_carts_archive_20260303;
+ALTER TABLE public.email_send_log RENAME TO email_send_log_archive_20260303;
+ALTER TABLE public.order_refunds RENAME TO order_refunds_archive_20260303;
+ALTER TABLE public.password_reset_tokens RENAME TO password_reset_tokens_archive_20260303;
+ALTER TABLE public.payments RENAME TO payments_archive_20260303;
+ALTER TABLE public.product_media RENAME TO product_media_archive_20260303;
+ALTER TABLE public.shipments RENAME TO shipments_archive_20260303;
+
+COMMIT;
